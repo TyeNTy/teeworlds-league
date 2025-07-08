11d2f92620b536eaa6e28e6baa0343fedc7c0dc0
TyeNTy <mathias.coussat@free.fr>
2025-07-08 22:53:28 +0200

    Fix

--- app/src/scenes/votes/detail.js
+++ app/src/scenes/votes/detail.js
@@ -152,7 +152,7 @@ const Detail = () => {
   return (
     <div className="p-4 max-w-2xl mx-auto">
       <div className="mb-6">
-        <h1 className="text-2xl font-bold">
+        <h1 className="text-2xl font-bold text-center">
           Edit Vote
         </h1>
       </div>
