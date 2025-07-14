4c2bdef7d3aea784039d09b161ec30db70f25f92
TyeNTy <mathias.coussat@free.fr>
2025-07-14 17:21:36 +0200

    Nez discord link

--- app/src/components/Footer.js
+++ app/src/components/Footer.js
@@ -5,8 +5,8 @@ const Footer = () => {
     <footer className="flex w-full flex-col justify-center border-t border-gray-300 bg-gray-800">
       <div className="flex w-full items-center justify-center divide-x divide-gray-300 px-20 py-4 text-sm text-white">
         Join our discord here :{" "}
-        <a href="https://pic.zcat.ch/" className="ml-2 text-blue-400">
-          https://pic.zcat.ch/
+        <a href="https://discord.gg/pmwP4gM8qC" className="ml-2 text-blue-400">
+        https://discord.gg/pmwP4gM8qC
         </a>
       </div>
     </footer>
